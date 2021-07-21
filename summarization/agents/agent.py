39c3f8f773046881aca8d92bc417b723f9a24a4a
HatemMorgan <hatemmorgan17@gmail.com>
2021-07-21 11:44:27 +0200

    Add agent abstract class

--- summarization/agents/agent.py
+++ summarization/agents/agent.py
@@ -1,7 +1,18 @@
-# from abc import ABC, abstractmethod
-#
-#
-# class Agent(ABC):
-#
-#     # @abstractmethod
-#     # def run(self, con):
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+
+class Agent(ABC):
+
+    @abstractmethod
+    def run_conv(self, conv_root: Dict) -> str:
+        pass
+
+    def run_all(self, conv_list: List[Dict[str, List[str]]]) -> Dict[str, str]:
+        summaries = {}  # conversation summaries
+        for conv_dict in conv_list:
+            conv_id, conv_root = next(iter(conv_dict.items()))
+            conv_summary = self.run_conv(conv_root)
+            summaries[conv_id] = conv_summary
+
+        return summaries
