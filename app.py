492782da889263a6fd97a21bb12ecdb2d99bf221
abilalakin <bilal97@gmail.com>
2024-06-20 20:55:22 +0200

    Remove fixed conversation count

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def fetch_summaries():
 def topic_aware_summarize():
     conversation_list = request.json["conversations"]
     topics_df, topic_embeddings = bertopic.get_topic_embeddings(conversation_list)
-    dict_topic_sentences = topic_aware_summarizer.extract_topic_sentences(conversation_list[:100], topics_df,
+    dict_topic_sentences = topic_aware_summarizer.extract_topic_sentences(conversation_list, topics_df,
                                                                           topic_embeddings)
     conv_summaries = summarizer_agent.run_all_topic_aware(dict_topic_sentences)
     return jsonify({"conv_summaries": conv_summaries})
@@ -114,6 +114,8 @@ def topic_aware_summarize():
 @app.route('/delta-summarize', methods=["POST"])
 def delta_summarize():
     summaries = request.json["summaries"]
+    #TODO: Return delta summarization plots.
+    return
 
 
 @app.route('/health', methods=["GET"])
