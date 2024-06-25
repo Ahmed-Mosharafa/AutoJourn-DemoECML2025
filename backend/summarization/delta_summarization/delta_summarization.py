ab65562f9356406828b667d49a9119836c8c5bcb
abilalakin <bilal97@gmail.com>
2024-06-25 22:07:53 +0200

    Remove dummy data

--- backend/summarization/delta_summarization/delta_summarization.py
+++ backend/summarization/delta_summarization/delta_summarization.py
@@ -5,18 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import io
 
-summary1 = '''A Bugatti Chiron, powerd by its turbocharged 8 liter engine with 16 cylinder4s and 1600 horse- 
-power, drove at record braking speeds of up to 417 
-kilometers per hour on a german motorway. With
-a top speed of 440 kilometers per hour, the sports
-car is one of the fastest legal road vehicles.'''
-summary2 = '''
-German politicians demand a general speed limit
-due to safety and environmental reasons after a
-car legally drove at a very high speed on a public
-road.
-'''
-
 
 class DeltaSummarization:
     def __init__(self):
@@ -85,14 +73,3 @@ class DeltaSummarization:
             img_buffer = self.create_2d_scatter_plot(img_buffer, topic_summaries)
 
         return img_buffer
-
-
-delta = DeltaSummarization()
-delta.find_cosine_similarity(summary1, summary2)
-topic_summaries = {
-    "0_you_to_it_the": "May is depressed and doesn't want to see anyone. Karen will call someone for advice.",
-    "2_doctor_and_to_the": "Adam and Karen worry about her. Karen suggested she should see a specialist. Adam has a friend who is a psychologist. ",
-    "7_the_is_ignorant_all": "Adam needs someone to call"
-}
-delta.create_cosine_similarity_matrix(topic_summaries)
-delta.create_2d_scatter_plot(topic_summaries)
